a950aecc546537331586d6a679610bd80c022d02
Hasham Sarwar <hsarwar4227@gmail.com>
2026-07-11 09:31:27 +0100

    fix sig issue 2

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -3,6 +3,6 @@ import stripeWebhook from "@/lib/payment/stripe/stripe_webhook";
 
 export async function POST(req: Request) {
     const secret = process.env.STRIPE_WEBHOOK_SECRET;
-    await stripeWebhook(secret!, req);
+    return stripeWebhook(secret!, req);
 
 }
